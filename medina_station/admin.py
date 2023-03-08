4f7d1cb972154577c5bd5f7ea2ff668a7a406bb3
Tim Davis <mitted2@gmail.com>
2023-03-07 17:49:26 -0700

    made CrewAdmin

--- medina_station/admin.py
+++ medina_station/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Crew)
+class CrewAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'origin', 'staff_level']
+    list_editable = ['staff_level']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name', 'origin', 'staff_level']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
--- medina_station/models.py
+++ medina_station/models.py
@@ -62,6 +62,7 @@ class Commander(models.Model):
     #user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     #name = models.CharField(max_length=60)
     alliance = models.CharField(
+        max_length=25,
         choices=Alliance.choices,
         default=Alliance.UNAFFILIATED
     )
